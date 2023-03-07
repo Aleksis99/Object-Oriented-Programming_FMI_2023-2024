0a99729b719f39bedab24c53b4e95b11d9eb72f8
Aleksis99 <49377192+Aleksis99@users.noreply.github.com>
2023-03-07 17:19:03 +0200

    Add files via upload

---
+++ Sem .02/task1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+
+const int MAX_SIZE = 1000;
+
+int main()
+{
+	std::ifstream file("task1");
+
+	if (!file.is_open())
+	{
+		std::cout << "Error";
+		return -1;
+	}
+
+	char buffer[MAX_SIZE];
+
+	while (!file.eof())
+	{
+		file.getline(buffer, MAX_SIZE);
+
+		std::cout << buffer << std::endl;
+	}
+
+	file.close();
+
+}
